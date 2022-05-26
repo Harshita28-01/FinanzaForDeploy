23f8220cd71345b5d1a47870118b9f3da6a23b50
Harshita28-01 <harshita.varshney.2801@gmail.com>
2022-05-26 22:58:38 +0530

    Login changed

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -38,7 +38,7 @@ function Login(props){
     function handleSubmit(event){
         event.preventDefault();
 
-        fetch("http://localhost:3001/log-in",{
+        fetch("/log-in",{
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
